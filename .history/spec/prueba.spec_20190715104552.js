9b8dce216ccc6ece78115f99ea74efae99cbc6f4
beltranrengifo <beltran@rengifo.es>
2019-07-15 11:02:44 +0200

    RM history from git

--- .history/spec/prueba.spec_20190715104552.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-describe('Prueba 1', () => {
-  console.log('Prueba 1')
-  it('should be zero', () => {
-    console.log('should be zero')
-    const foo = 0
-    expect(foo).toBe(0)
-  })
-})
